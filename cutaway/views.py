f328c06afaadd8753a74ad26f95126a0b97fd6cb
Andy1407 <58524040+Andy1407@users.noreply.github.com>
2020-01-15 20:40:17 +0300

    Update views.py

--- cutaway/views.py
+++ cutaway/views.py
@@ -4,13 +4,5 @@ from .models import MyExperience
 
 
 def basic(request):
-    my_experience = get_object_or_404(MyExperience)
-    return render(request, 'cutaway/basic.html', {'my_experience': my_experience})
-
-
-def add(request):
-    my_add = get_object_or_404(MyExperience)
-    return render(request, 'cutaway/add.html', {'my_add': my_add})
-
-
-# Create your views here.
+    basic = get_object_or_404(MyExperience)
+    return render(request, 'cutaway/basic.html', {'basic': basic})
