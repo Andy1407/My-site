d88ec1d0eba5d461c7221ae1ab984772f879c402
Andy1407 <58524040+Andy1407@users.noreply.github.com>
2020-01-18 18:28:47 +0300

    Update wsgi.py

--- AboutMe/wsgi.py
+++ AboutMe/wsgi.py
@@ -1,8 +1,8 @@
 import os
-import sys
+
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'AboutMe.settings'
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+
