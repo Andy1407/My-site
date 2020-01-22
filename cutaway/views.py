34bbf9ba31224fc1abc3383e6557002d5460596a
Andy1407 <58524040+Andy1407@users.noreply.github.com>
2020-01-22 17:33:55 +0300

    Delete views.py

--- cutaway/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-
-from .models import MyExperience
-
-
-def basic(request):
-    basic = get_object_or_404(MyExperience)
-    return render(request, 'cutaway/basic.html', {'basic': basic})
