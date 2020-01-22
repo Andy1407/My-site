a7f0d9f7e06e26b19fed470406720ca8beb22587
Andy1407 <58524040+Andy1407@users.noreply.github.com>
2020-01-22 17:34:10 +0300

    Delete urls.py

--- cutaway/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-from . import views
-
-app_name = 'cutaway'
-urlpatterns = [
-    path('', views.basic, name='basic'),
-] + static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
